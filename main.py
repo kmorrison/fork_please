1a8f2926a784fd1600440bfc31a4598bda65c439
Kyle Morrison <kylem@yelp.com>
2015-11-12 23:02:26 -0800

    Some frontends

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from flask import Flask
 from flask_wtf import Form
 from flask_wtf.file import FileField
 from flask_wtf.file import FileAllowed
+from wtforms import BooleanField
 from wtforms import StringField
 from wtforms.validators import DataRequired
 from google.appengine.api import users
@@ -26,15 +27,6 @@ app = Flask(__name__)
 # the App Engine WSGI application server.
 
 APP_NAME = 'fork_please'
-
-
-@app.route('/')
-def home():
-    """Return a friendly HTTP greeting."""
-    return flask.render_template(
-        'home.html',
-        title=APP_NAME,
-    )
 
 
 @app.route('/me')
@@ -59,6 +51,14 @@ class EventUploadForm(Form):
         ],
     )
 
+class ToggleForm(Form):
+
+    event = StringField('Event Name', validators=[DataRequired()])
+    name = StringField(
+        'Name',
+    )
+    is_checked = BooleanField()
+
 def upsert_event(name):
     event_slug = slugify.slugify(name)
     event = models.Event.get_or_insert(
@@ -77,6 +77,14 @@ def upsert_attendee(event_name, attendee_name):
         event_name=event_name,
     )
     attendee.put()
+
+
+@app.route('/', methods=('GET',))
+def home():
+    return flask.render_template(
+        'events.html',
+        events=models.Event.query().order(-models.Event.date).fetch(),
+    )
 
 
 @app.route('/upload/', methods=('GET', 'POST'))
@@ -108,16 +116,35 @@ def upload():
     )
 
 @app.route('/event/<event_slug>/', methods=('GET', ))
+@login.login_required
+@login.company_login_required
 def event_page(event_slug):
     attendees = models.Attendee.query(models.Attendee.event_name==event_slug).order(models.Attendee.name).fetch()
     total_attendees = len(attendees)
     checked_attendees = sum(attendee.is_checked for attendee in attendees)
     return flask.render_template(
         'event.html',
+        toggle_form=ToggleForm(),
+        event_slug=event_slug,
         attendees=attendees,
         total_attendees=total_attendees,
         checked_attendees=checked_attendees,
     )
+
+@app.route('/event/toggle_attendance/', methods=('POST', ))
+def toggle_attendance():
+    form = ToggleForm()
+    if form.validate_on_submit():
+        attendees = models.Attendee.query(
+            models.Attendee.event_name == form.event.data,
+            models.Attendee.name == form.name.data,
+        ).fetch()
+        attendee = attendees[0]
+        attendee.is_checked = form.is_checked.data
+        attendee.put()
+    else:
+        print form.errors
+    return flask.redirect('event/%s' % form.event.data)
 
 
 @app.errorhandler(404)
